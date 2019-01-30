23572431ad49a111a36573a110d9faa1408c5288
Akili Humphrey <akilihumphrey@gmail.com>
2019-01-30 06:51:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,9 +27,18 @@ class Student
     end
   end
 
-def self.students_below_12th_grade
+  def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students WHERE grade BETWEEN 9 and 11
+    SQL
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end
+  end
+  
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10
     SQL
     DB[:conn].execute(sql).map do |row|
     self.new_from_db(row)
