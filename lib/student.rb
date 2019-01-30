3eabd7f0f9485c7e50e4847d0c1889aaeca9e503
Akili Humphrey <akilihumphrey@gmail.com>
2019-01-30 06:54:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,15 @@ class Student
     end
   end
   
+  def self.all_students_in_grade_X
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+    end
+  end
+  
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT * FROM students WHERE name = ? LIMIT 1
