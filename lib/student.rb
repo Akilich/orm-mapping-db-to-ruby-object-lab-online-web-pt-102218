13e4be8d2ba8c820d24a4239e4732f7ce4f9d62a
Akili Humphrey <akilihumphrey@gmail.com>
2019-01-30 06:44:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
   end
   
   def self.all_students_in_grade_9
-    ql = <<-SQL
+    sql = <<-SQL
       SELECT * FROM students WHERE grade = 9
     SQL
     DB[:conn].execute(sql).map do |row|
